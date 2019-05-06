1095492c81e392d0315767b92a3a678f9134500c
GardianT <1642271724@qq.com>
2019-05-06 10:22:29 +0800

    update

---
+++ src/189-rotate/rotate.py
@@ -0,0 +1,30 @@
+# Q:
+# 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+# 示例 1:
+# 输入: [1,2,3,4,5,6,7] 和 k = 3
+# 输出: [5,6,7,1,2,3,4]
+# 解释:
+# 向右旋转 1 步: [7,1,2,3,4,5,6]
+# 向右旋转 2 步: [6,7,1,2,3,4,5]
+# 向右旋转 3 步: [5,6,7,1,2,3,4]
+# 示例 2:
+# 输入: [-1,-100,3,99] 和 k = 2
+# 输出: [3,99,-1,-100]
+# 解释: 
+# 向右旋转 1 步: [99,-1,-100,3]
+# 向右旋转 2 步: [3,99,-1,-100]
+def reverse(target, m, n):
+    for i in range((n + 1 - m) / 2):
+        target[m + i], target[n - i] = target[n - i], target[m + i]
+        
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        reverse(nums, 0, len(nums) - 1)
+        reverse(nums, 0, k - 1)
+        reverse(nums, k, len(nums) - 1)
